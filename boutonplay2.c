d5d0aa228b562a4549f07b3c2df34e8e7221cc3d
NathanIsoardi <125483922+NathanIsoardi@users.noreply.github.com>
2023-03-12 17:05:47 +0100

    Add files via upload

---
+++ boutonplay2.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+int main(int argc, char* argv[]) {
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow("Images", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, 0);
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+    SDL_Surface* image1 = IMG_Load("play1.png");
+    SDL_Texture* texture1 = SDL_CreateTextureFromSurface(renderer, image1);
+    SDL_FreeSurface(image1);
+
+    SDL_Surface* image2 = IMG_Load("play2.png");
+    SDL_Texture* texture2 = SDL_CreateTextureFromSurface(renderer, image2);
+    SDL_FreeSurface(image2);
+
+    SDL_Surface* image3 = SDL_LoadBMP("play3.bmp");
+    SDL_Texture* texture3 = SDL_CreateTextureFromSurface(renderer, image3);
+    SDL_FreeSurface(image3);
+
+    SDL_Surface* image4 = SDL_LoadBMP("vert.bmp");
+    SDL_Texture* texture4 = SDL_CreateTextureFromSurface(renderer, image4);
+    SDL_FreeSurface(image4);
+
+    SDL_Rect imageRect1 = {50, 50, 200, 200};
+    SDL_Color color1 = {255, 255, 255, 255};
+
+    SDL_Texture* currentImage = texture1;
+
+    SDL_bool done = SDL_FALSE;
+
+    while (!done) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    done = SDL_TRUE;
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    if (SDL_PointInRect(&(SDL_Point){event.button.x, event.button.y}, &imageRect1)) {
+                        printf("Image 1 cliquée\n");
+                        currentImage = texture3;
+                    }
+                    break;
+                case SDL_MOUSEMOTION:
+                    if (SDL_PointInRect(&(SDL_Point){event.motion.x, event.motion.y}, &imageRect1)) {
+                        currentImage = texture2;
+                    } else {
+                        currentImage = texture1;
+                    }
+                    break;
+            }
+        }
+
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture4, NULL, NULL); // Afficher l'image verte en arrière-plan
+        SDL_RenderCopy(renderer, currentImage, NULL, &imageRect1);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyTexture(texture1);
+    SDL_DestroyTexture(texture2);
+    SDL_DestroyTexture(texture3);
+    SDL_DestroyTexture(texture4);  // Libérer la texture de l'image verte
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
+
