2dcca6bf3be532d10f872bbefe61d4677c1c2399
Matrix172 <127189157+Matrix172@users.noreply.github.com>
2023-04-18 10:42:00 +0200

    Update Test7.c
    Incomplete but better...

--- test6.c
+++ test6.c
@@ -7,8 +7,6 @@
 #include "init.c"
 
 // On initialise les pions blancs et noirs sur le plateau.
-int clic = 0;
-
 int main(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_VIDEO);
@@ -67,19 +65,20 @@ int main(int argc, char *argv[])
 
     SDL_Texture *pcurim[14];
     SDL_Texture *pcurimb[14];
-    SDL_Texture *pcurimbleu[14];
     for (int i = 0; i < 14; i++)
     {
         SDL_Texture *currentImage = ptex[i];
         pcurim[i] = ptex[i];
         SDL_Texture *currentImageb = ptexb[i];
         pcurimb[i] = ptexb[i];
-        SDL_Texture *currentImagebleu = ptexbleu[i];
-        pcurimbleu[i] = ptexbleu[i];
     }
 
     SDL_bool done = SDL_FALSE;
     SDL_bool imageClicked1 = SDL_FALSE;
+
+    int i = 0;
+    int j = 0;
+    int compteur = 0;
 
     while (!done)
     {
@@ -88,34 +87,60 @@ int main(int argc, char *argv[])
         {
             switch (event.type)
             {
+
             case SDL_QUIT: // On ferme la fenètre
                 done = SDL_TRUE;
                 break;
 
             case SDL_MOUSEBUTTONDOWN:
-                int i=0;
-                
-                for (i=0; i < 14; i++)
-                {
-                    int clic = 0;
-                    if (((pimrectb[0][i]).x <= event.button.x) && ((pimrectb[1][i]).y <= event.button.y))
-                    {
-                        printf("Clicked %d at x=%d, y=%d\n", i, event.button.x, event.button.y); // afficher les coordonnées du clic
-                        imageClicked1 = SDL_TRUE;
-                    }
 
-                    if (clic % 2 == 0)
+                // recup le i de la boule sur laquelle on clique
+                int x1;
+                int y1;
+                int x2;
+                int y2;
+                x1 = event.button.x;
+                y1 = event.button.y;
+                x2 = event.button.x;
+                y2 = event.button.y;
+
+                // Pour chaque emplacement de boules cliquées, on regarde le numéro de la boule et on le met dans i.
+                // Detecte la k-ième boule du tableau
+
+                for (int k = 0; k < 14; k++)
+                {
+                    if ((((conoire[0][k])) > (x1 - 35)) && (((conoire[0][k])) <= (x1 + 35)))
+                    {
+                        if ((((conoire[1][k]) - 35) > (y1 - 35)) && (((conoire[1][k]) - 35) <= (y1 + 35)))
+                        {
+                            i = k;
+                        }
+                    }
+                }
+
+                // On compare la boule et le clic et si c'est dedans...
+                // on change la couleur de la bille correspondante
+                if ((((conoire[0][i]) > (x1 - 35)) && ((conoire[0][i]) <= (x1 + 35))) && (((conoire[1][i]) > (y1 - 35)) && ((conoire[1][i]) <= (y1 + 35))))
+                {
+                    if ((pcurim[i]) == (ptexbleu[i]))
+                    {
+                        pcurim[i] = ptex[i];
+                        compteur--;
+                        printf("%d\n", compteur);
+                    }
+                    else
+                    {
+                        if ((compteur >= 3) || (compteur < 0))
+                        {
+                            printf("Coup impossible\n");
+                        }
+                        else
                         {
                             pcurim[i] = ptexbleu[i];
-                            clic++;
+                            compteur++;
+                            printf("%d\n", compteur);
                         }
-                        else if (clic % 2 == 1)
-                        {
-                            pcurim[i] = ptex[i];
-                            clic++;
-                        }
-
-                    break;
+                    }
                 }
             }
         }
