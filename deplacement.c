e375b2a00e52cca3f4baafb1f476e60002291036
Nanahide17 <102522276+Nanahide17@users.noreply.github.com>
2023-04-29 19:51:01 +0200

    Add files via upload

--- deplacement.c
+++ deplacement.c
@@ -11,7 +11,8 @@
 ///Et le cas où il y a une bille sur la case de destination
 bool DeplacePion(DIRECTION direction, CASE* case_a_deplacer, TYPE_CASE couleur_joueur, int nb_meme_bille_derriere, int nb_bille_ennemi)
 {
-    //TODO : stocker couleur du joueur, faire fonction qui prend direction et case à deplacer qui va initialiser nb meme bille et nb bille ennemi
+    //TODO : fonction qui détermine la direction
+    //TODO : faire fonction qui prend direction et case à deplacer qui va initialiser nb meme bille et nb bille ennemi
     CASE* case_de_destination;
     bool deplacement_possible;
 
@@ -48,9 +49,14 @@ bool DeplacePion(DIRECTION direction, CASE* case_a_deplacer, TYPE_CASE couleur_j
     //TODO : Gérer cas EJECTE return true
     case VIDE:
         //La case de destination prend la couleur de la case à déplacer et on vide la case à déplacer
-        case_de_destination->couleur=case_a_deplacer->couleur;
-        case_a_deplacer->couleur=VIDE;
-        return true;
+        if(nb_meme_bille_derriere>nb_bille_ennemi){ //Vérifie si le nombre de bille ennemi n'est pas supérieur au nombre de bille du joueur
+            case_de_destination->couleur=case_a_deplacer->couleur;
+            case_a_deplacer->couleur=VIDE;
+            return true;
+        }
+        else{
+            return false;
+        }
         break;
 
     //On a une bille en face
@@ -96,4 +102,51 @@ bool DeplacePion(DIRECTION direction, CASE* case_a_deplacer, TYPE_CASE couleur_j
         //TODO : Gerer le cas où pas assez de bille pour pousser l'ennemi (car pour l'instant si la case derrière l'ennemi est vide, on peut jouer)
         break;
     }
+    return false;
+}
+
+
+///On retourne la DIRECTION dans laquelle l'utilisateur veut aller
+///On prend en argument la première case de déplacement et la case de destination
+DIRECTION trouveDirection(CASE* emplacement_bille, CASE* destination){
+    int x_bille = emplacement_bille->posX, y_bille= emplacement_bille->posY;
+    int x_destination = destination->posX, y_destination = destination->posY;
+    float x,y;
+
+    x=x_bille/x_destination;
+    y=y_bille/y_destination;
+
+    if(x>1){
+    //Si l'utilisateur veut aller vers O
+        if(y>1){
+        //Si l'utilisateur veut aller vers NO
+            return NO;
+        }
+
+        else if(y==1){
+        //Si l'utilisateur veut aller pleine O
+            return O;
+        }
+        else if(y<1){
+        //Si l'utilisateur veut aller vers SO
+            return SO;
+        }
+    }
+    if(x<1){
+    //Si l'utilisateur veut aller vers E
+        if(y>1){
+        //Si l'utilisateur veut aller vers NE
+            return NE;
+        }
+
+        else if(y==1){
+        //Si l'utilisateur veut aller pleine E
+            return E;
+        }
+        else if(y<1){
+        //Si l'utilisateur veut aller vers SE
+            return SE;
+        }
+    }
+    return EXIT_FAILURE;
 }
