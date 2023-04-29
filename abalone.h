f83623ad6b7a1c19d94aefde2847ab9f8094424d
Nanahide17 <102522276+Nanahide17@users.noreply.github.com>
2023-04-29 19:50:38 +0200

    Add files via upload

--- abalone.h
+++ abalone.h
@@ -1,16 +1,18 @@
 #ifndef ABALONE_H_INCLUDED
 #define ABALONE_H_INCLUDED
 
+#include <stdbool.h>
 //Definitions des deux joueurs
-enum joueur{VIDE, NOIR, BLANC,EJECTE};
+typedef enum joueur{VIDE, NOIR, BLANC, EJECTE}TYPE_CASE;
 
 //Création d'un pion avec la couleur, le numéro du pion et ces alentours
 typedef struct jeu{
-    enum joueur couleur;
-    int posX, posY;
+    TYPE_CASE couleur;
+    int posX, posY; //posX et posY sont en pixels
     struct coordonnées *alentours;
 }CASE;
 
+typedef enum direction{NO,O,SO,NE,E,SE}DIRECTION;
 
 typedef struct coordonnées{
     CASE *NO;
@@ -39,6 +41,12 @@ void alentour();
 void initialisationPile(PILE* pil, CASE* casse);
 void reliageBas(PILE* pile1, PILE* pile2);
 void reliageHaut(PILE* pile1, PILE* pile2);
+
+//Fonctions déplacement
+bool DeplacePion(DIRECTION direction, CASE* case_a_deplacer, TYPE_CASE couleur_joueur, int nb_meme_bille_derriere, int nb_bille_ennemi);
+DIRECTION trouveDirection(CASE* emplacement_bille, CASE* destination);
+CASE* trouveCASE(int X, int Y);
+bool isInsideCASE(int X, int Y, CASE* casse);
 //Fonctions SDL
 
 
